3b46c7a634ec1c5f5a50497e6890b3d36370f859
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2019-04-21 18:23:00 +0530

    Create HomeController.cs

---
+++ EMall/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+
+namespace Emall.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+        
+            return View();
+        }
+  
+        public ActionResult Kitchen()
+        {
+            return View();
+        }
+        public ActionResult Care()
+        {
+            return View();
+        }
+        public ActionResult Shipping()
+        {
+            return View();
+
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
